a5c0df254112cfe7dae567891abad128278486a6
Coach Mack <jmckellips@wpsok.org>
2026-09-02 13:16:15 -0500

    Added HW1-HW4 review notes

--- HW3.py
+++ HW3.py
@@ -1,25 +1,31 @@
-#Name:
+#Name: Coach Mack
 #Class: 5th Hour
 #Assignment: HW3
 
 
 #1. Print "Hello World!"
-
+print("Hello World!")
 #2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
-
+my_int = 68
+my_str = "Mack"
+my_bool = True
 #3. Print all three variables on the same print function (at the same time).
-
+print(my_int, my_str, my_bool)
 #4. Create a variable that asks the user to input an integer.
-
+user_int = int(input("Enter an integer: "))
 #5. Add the integer variable from #2 with the integer from #4 and print the result.
-
+int_sum = my_int + user_int
+print(int_sum)
 #6. Take the result from #5 and divide it by 2. Print the result.
-
+divide_sum_by_2 = int_sum / 2
+print(divide_sum_by_2)
 #7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
-
+my_bool = False
 #8. Print the value of the boolean variable.
-
+print(my_bool)
 #9. Create a variable with a number that contains decimals.
-
+my_float = 2.75
 #10. Round the number from #9 up or down using the round function.
+round_my_float = round(my_float)
+print(round_my_float)
 
